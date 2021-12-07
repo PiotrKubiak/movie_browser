ab1ead527388fd67e01bda398918a124ebe1ac01
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-12-07 16:28:09 +0100

    Add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import MoviesList from "./features/MoviesList";
+import { StyledInput, StyledList, StyledNav, StyledNavLink, StyledSearch } from "./styled";
 
 function App() {
   return (
-    <div>
-      hejka
-    </div>
+    <HashRouter>
+      <StyledNav>
+        <StyledList>
+          <li>🎥 Movies Browser</li>
+          <li><StyledNavLink to="/movies">MOVIES</StyledNavLink></li>
+          <li><StyledNavLink to="/people">PEOPLE</StyledNavLink></li>
+          <li><StyledSearch><StyledInput placeholder="🔍︎ Search for movies..."></StyledInput></StyledSearch></li>
+        </StyledList>
+      </StyledNav>
+      <Switch>
+        <Route path="/movies">
+          <MoviesList />
+        </Route>
+        <Route path="/people"></Route>
+        <Redirect to="/movies" />
+      </Switch>
+    </HashRouter>
   );
 }
 
