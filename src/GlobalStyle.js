926eabcf0edf00820996e6bec3c1d176526682f9
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-12-07 10:56:50 +0100

    add font-family

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -11,6 +11,6 @@ export const GlobalStyle = createGlobalStyle`
 
     body {
         background-color: ${({ theme }) => theme.color.gallery};
-        font-family: 'Montserrat', sans-serif;
+        font-family: 'Poppins', sans-serif;
     }
     `;
