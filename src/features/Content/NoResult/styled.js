ecfab298496907438d5464519563d023e1b67a36
palel <32599663+palel@users.noreply.github.com>
2021-12-10 21:58:08 +0100

    styles for noResult component

---
+++ src/features/Content/NoResult/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+// import { ReactComponent as NoResultImage } from "./NoResult.png";
+
+export const Wrapper = styled.div`
+    max-width: 1368px;
+    margin: auto;
+    padding-top: 56px;
+
+`;
+
+export const Header = styled.header`
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 1.2;
+`;
+
+export const Image = styled.img`
+    display: block;
+    margin: 40px auto;
+    max-width: 668px;
+`;
