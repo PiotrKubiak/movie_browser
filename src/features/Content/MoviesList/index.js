2c89c8b5162848932bf1cc4b7c9bbff8be3d5b52
palel <32599663+palel@users.noreply.github.com>
2021-12-10 21:58:33 +0100

    no result usage /*commented*/

--- src/features/Content/MoviesList/index.js
+++ src/features/Content/MoviesList/index.js
@@ -1,10 +1,13 @@
 import { StyledHeader, StyledSection, StyledTiles } from "./styled";
 import Tile  from "./Tile";
 import { Pagination } from "../../../common/Pagination";
+import NoResult from "../NoResult";
 
 const MoviesList = () => {
     return (
         <StyledSection>
+            {/* <NoResult/> */}
+            
             <StyledHeader>Popular movies</StyledHeader>
             <StyledTiles>
                 <Tile />
