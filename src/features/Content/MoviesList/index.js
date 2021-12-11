46d85af09c57d6d4e8dd61f94c34e8f95ab77584
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-12-11 14:31:12 +0100

    initial commit

--- src/features/Content/MoviesList/index.js
+++ src/features/Content/MoviesList/index.js
@@ -1,9 +1,26 @@
 import { StyledHeader, StyledSection, StyledTiles } from "./styled";
 import Tile  from "./Tile";
 import { Pagination } from "../../../common/Pagination";
-import NoResult from "../../../common/NoResult";
+import { getMovies } from "./movieBrowserApi.js";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { fetchMovies, selectMovies, selectMoviesStatus } from "../../movieBrowserSlice";
+import { useEffect } from "react";
+// import NoResult from "../../../common/NoResult";
 
 const MoviesList = () => {
+    const dispatch = useDispatch();
+
+    const moviesStatus = useSelector(selectMoviesStatus);
+    const movies = useSelector(selectMovies);
+
+    useEffect(() => {
+        dispatch(fetchMovies());
+    }, [dispatch]);
+
+
+    // const movies = getMovies();
+
     return (
         <StyledSection>
             {/* <NoResult/> */}
