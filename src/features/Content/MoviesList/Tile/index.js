8e5c99e5fa07470a82c79e637b30eeb16e57101c
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-12-12 11:14:24 +0100

    imports in Tile

--- src/features/Content/MoviesList/Tile/index.js
+++ src/features/Content/MoviesList/Tile/index.js
@@ -1,26 +1,38 @@
-import { Container, Description, Image, Rate, Rating, Star, Tag, Tags, Title, Votes, Year } from "./styled";
-import poster from "../../../images/poster.png";
+import {
+  Container,
+  Description,
+  Image,
+  Rate,
+  Rating,
+  Star,
+  Tag,
+  Tags,
+  Title,
+  Votes,
+  Year,
+} from "./styled";
+import poster from "../../../../images/poster.png";
 
 const Tile = () => {
-    return (
-        <Container>
-            <Image src={poster} alt="" />
-            <Description>
-                <Title>Mulan</Title>
-                <Year>2020</Year>
-                <Tags>
-                    <Tag>Action</Tag>
-                    <Tag>Adventure</Tag>
-                    <Tag>Drama</Tag>
-                </Tags>
-                <Rating>
-                    <Star />
-                    <Rate>7.8</Rate>
-                    <Votes>35 Votes</Votes>
-                </Rating>
-            </Description>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Image src={poster} alt="" />
+      <Description>
+        <Title>Mulan</Title>
+        <Year>2020</Year>
+        <Tags>
+          <Tag>Action</Tag>
+          <Tag>Adventure</Tag>
+          <Tag>Drama</Tag>
+        </Tags>
+        <Rating>
+          <Star />
+          <Rate>7.8</Rate>
+          <Votes>35 Votes</Votes>
+        </Rating>
+      </Description>
+    </Container>
+  );
+};
 
 export default Tile;
