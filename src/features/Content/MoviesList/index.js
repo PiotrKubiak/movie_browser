bbbdf86351d3b8db7a483ead7a4addd1098f1b5c
palel <32599663+palel@users.noreply.github.com>
2021-12-12 11:14:34 +0100

    Tile component using api

--- src/features/Content/MoviesList/index.js
+++ src/features/Content/MoviesList/index.js
@@ -1,29 +1,34 @@
 import { StyledHeader, StyledSection, StyledTiles } from "./styled";
-import Tile  from "./Tile";
 import { Pagination } from "../../../common/Pagination";
-import NoResult from "../../../common/NoResult";
+// import NoResult from "../../../common/NoResult";
+
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMovies, selectMovies, selectMovieStatus } from "./moviesBrowserSlice";
+import { useEffect } from "react";
+import { Content } from "./content";
+
+
 
 const MoviesList = () => {
+    const dispatch = useDispatch();
+    const moviesStatus = useSelector(selectMovieStatus);
+    const movies = useSelector(selectMovies);
+
+    useEffect(() => {
+        dispatch(fetchMovies());
+    }, [dispatch]);
+
     return (
         <StyledSection>
             {/* <NoResult/> */}
-            
+
             <StyledHeader>Popular movies</StyledHeader>
             <StyledTiles>
-                <Tile />
-                <Tile />
-                <Tile />
-                <Tile />
-                <Tile />
-                <Tile />
-                <Tile />
-                <Tile />
+                <Content moviesStatus={moviesStatus} movies={movies} />
             </StyledTiles>
             <Pagination />
         </StyledSection>
-  
-  );
+    );
 };
-
 
 export default MoviesList;
