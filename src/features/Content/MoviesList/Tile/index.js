2ade6fa4a4f025003b3b9197c3ff70f2d2c57096
palel <32599663+palel@users.noreply.github.com>
2021-12-12 11:14:15 +0100

    Tile component using api

--- src/features/Content/MoviesList/Tile/index.js
+++ src/features/Content/MoviesList/Tile/index.js
@@ -1,26 +1,31 @@
 import { Container, Description, Image, Rate, Rating, Star, Tag, Tags, Title, Votes, Year } from "./styled";
-import poster from "../../../images/poster.png";
+import { img_base_url } from "../moviesBrowserApi";
 
-const Tile = () => {
+
+const Tile = ({ movies }) => {
+
     return (
-        <Container>
-            <Image src={poster} alt="" />
-            <Description>
-                <Title>Mulan</Title>
-                <Year>2020</Year>
-                <Tags>
-                    <Tag>Action</Tag>
-                    <Tag>Adventure</Tag>
-                    <Tag>Drama</Tag>
-                </Tags>
-                <Rating>
-                    <Star />
-                    <Rate>7.8</Rate>
-                    <Votes>35 Votes</Votes>
-                </Rating>
-            </Description>
-        </Container>
-    )
-}
+        <>
+            {movies && movies.map(({ id, poster_path, original_title, release_date, genre_ids, vote_average, vote_count }) => (
+                <Container key={id}>
+                    <Image src={img_base_url + poster_path} alt={original_title} />
+                    <Description>
+                        <Title> {original_title} </Title>
+                        <Year>{release_date.substring(4,-1)}</Year>
+                        <Tags>
+                            <Tag>{genre_ids}</Tag>
+                        </Tags>
+
+                        <Rating>
+                            <Star />
+                            <Rate>{vote_average}</Rate>
+                            <Votes>{vote_count} votes</Votes>
+                        </Rating>
+                    </Description>
+                </Container>
+            ))};
+        </>
+    );
+};
 
 export default Tile;
