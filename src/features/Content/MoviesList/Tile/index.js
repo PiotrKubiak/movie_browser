190e050e24ac3e5b7db6c5ba53b7a51234db524c
palel <32599663+palel@users.noreply.github.com>
2021-12-14 23:12:47 +0100

    Information for Description and Rating components

--- src/features/Content/MoviesList/Tile/index.js
+++ src/features/Content/MoviesList/Tile/index.js
@@ -1,4 +1,4 @@
-import { Container, Description, Image, Rate, Rating, Star, Tag, Tags, Title, Votes, Year } from "../../styledTile";
+import { Container, Description, Image, Information, Rate, Rating, Star, Tag, Tags, Title, Votes, Year } from "../../styledTile";
 import { img_base_url } from "../../moviesBrowserApi";
 
 
@@ -9,19 +9,20 @@ const Tile = ({ movieBrowser }) => {
             {movieBrowser.map(({ id, poster_path, original_title, release_date, genre_ids, vote_average, vote_count }) => (
                 <Container key={id}>
                     <Image src={img_base_url + poster_path} alt={original_title} />
-                    <Description>
-                        <Title> {original_title} </Title>
-                        {/* <Year>{release_date.substring(4,-1)}</Year> */}
-                        <Tags>
-                            <Tag>{genre_ids}</Tag>
-                        </Tags>
-
+                    <Information>
+                        <Description>
+                            <Title> {original_title} </Title>
+                            {/* <Year>{release_date.substring(4, -1)}</Year> */}
+                            <Tags>
+                                <Tag>{genre_ids}</Tag>
+                            </Tags>
+                        </Description>
                         <Rating>
                             <Star />
                             <Rate>{vote_average}</Rate>
                             <Votes>{vote_count} votes</Votes>
                         </Rating>
-                    </Description>
+                    </Information>
                 </Container>
             ))};
         </>
