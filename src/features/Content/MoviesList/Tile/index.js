46d5e77502b14bb8413ec0acaf3f79caaa73d8af
palel <32599663+palel@users.noreply.github.com>
2021-12-14 01:13:05 +0100

    tworzenie tagów za pomocą map with genres array

--- src/features/Content/MoviesList/Tile/index.js
+++ src/features/Content/MoviesList/Tile/index.js
@@ -1,21 +1,22 @@
 import { Container, Description, Image, Rate, Rating, Star, Tag, Tags, Title, Votes, Year } from "./styled";
 import { img_base_url } from "../moviesBrowserApi";
+import { foundName } from "../getGenres";
 
-
-const Tile = ({ movies }) => {
+const Tile = ({ movies, genres }) => {
 
     return (
         <>
-            {movies.map(({ id, poster_path, original_title, release_date, genre_ids, vote_average, vote_count }) => (
+            {movies.map(({ id, poster_path, original_title, release_date, genre_ids = [], vote_average, vote_count }) => (
                 <Container key={id}>
                     <Image src={img_base_url + poster_path} alt={original_title} />
                     <Description>
                         <Title> {original_title} </Title>
-                        <Year>{release_date.substring(4,-1)}</Year>
+                        <Year>{release_date.substring(4, -1)}</Year>
                         <Tags>
-                            <Tag>{genre_ids}</Tag>
+                            {genre_ids.map((id, index) => {
+                                return <Tag key={index}>{foundName(id, genres)}</Tag>
+                            })}
                         </Tags>
-
                         <Rating>
                             <Star />
                             <Rate>{vote_average}</Rate>
@@ -23,7 +24,8 @@ const Tile = ({ movies }) => {
                         </Rating>
                     </Description>
                 </Container>
-            ))};
+            ))
+            };
         </>
     );
 };
