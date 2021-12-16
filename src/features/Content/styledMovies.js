b771baa8ff22c89332d79bd719d856a13028d365
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-12-16 14:45:06 +0100

    improvement of styledMovies

--- src/features/Content/styledMovies.js
+++ src/features/Content/styledMovies.js
@@ -1,35 +1,35 @@
 import styled from "styled-components";
 
 export const StyledSection = styled.div`
-    margin: auto;
-    max-width: 1368px;
-    max-height: 1391px;
+  margin: auto;
+  max-width: 1368px;
+  max-height: 1391px;
 `;
 
 export const StyledHeader = styled.h1`
-    margin: 56px 0px 24px 0px ;
-    font-size: 18px;
-    font-weight: 600;
-    font-size: 36px;
-    line-height: 1.2;
+  margin: 56px 0px 24px 0px;
+  font-size: 18px;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 1.2;
 
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        margin: 24px 0px 12px 0px ;
-        font-size: 36px;
-        font-weight: 600;
-        font-size: 18px;
-        line-height: 1.2;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+    margin: 24px 0px 12px 16px;
+    font-size: 36px;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 1.2;
+  }
 `;
 
 export const StyledTiles = styled.section`
-    margin: 0px;
-    display: grid;
-    grid-template-columns: repeat(4, auto) ;
-    grid-gap: 24px;
+  margin: 0px;
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+  grid-gap: 24px;
 
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        grid-template-columns: 1fr;
-        grid-gap: 16px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+    grid-template-columns: 1fr;
+    grid-gap: 16px;
+  }
 `;
