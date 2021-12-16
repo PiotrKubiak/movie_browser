831974327652edbd2a4fdaffa6b08c1aca714c5a
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-12-16 15:31:44 +0100

    new styled of Pagination

--- src/common/Pagination/styled.js
+++ src/common/Pagination/styled.js
@@ -1,19 +1,27 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  margin: 40px auto 100px;
+  margin: 40px auto;
+  padding-bottom: 103px;
   display: flex;
   justify-content: center;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+    margin: 16px auto 16px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
 export const Button = styled.button`
+  cursor: pointer;
   margin: 0 6px;
-  padding: 8px 16px;
+  padding: 4px 8px;
   background: ${({ theme }) => theme.color.pattensBlue};
   color: ${({ theme }) => theme.color.woodsmoke};
   border-radius: 5px;
   border: none;
-  cursor: pointer;
   font-size: 14px;
   &:hover {
     filter: brihtness(80%);
@@ -21,26 +29,38 @@ export const Button = styled.button`
   &:disabled {
     background: ${({ theme }) => theme.color.mystic};
     color: ${({ theme }) => theme.color.woodsmoke};
-    cursor: default;
+    cursor: not-allowed;
     filter: brightness(100%);
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+    padding: 8px 16px;
+    margin: 0px 8px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    & disabled {
+      background: ${({ theme }) => theme.color.mystic};
+      color: ${({ theme }) => theme.color.woodsmoke};
+      filter: brightness(100%);
+      cursor: not-allowed;
+    }
   }
 `;
 export const PaginationText = styled.span`
   font-size: 16px;
   color: ${({ theme }) => theme.color.waterloo};
-  line-height: 24px;
   align-self: center;
-  margin: 0px 8px;
-  @media (max-width: ${({ theme }) => theme.breakpoint.tableMax}) {
+  margin: 0px 4px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.tabletMax}) {
     font-size: 14px;
   }
-  @media (max-width: ${({ theme }) => theme.breakpoint.tableMax}) {
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
     font-size: 12px;
   }
-  @media (max-width: ${({ theme }) => theme.breakpoint.tableMin}) {
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMin}) {
     font-size: 10px;
     margin: 0 2px;
-    line-height: 24px;
     font-weight: 600;
   }
 `;
@@ -48,22 +68,20 @@ export const PaginationNumber = styled.span`
   font-size: 16px;
   color: ${({ theme }) => theme.color.woodsmoke};
   font-weight: 600;
-  line-height: 24px;
   align-self: center;
-  margin: 0px 8px;
+  margin: 0px 4px;
   font-size: 16px;
   @media (max-width: ${({ theme }) => theme.breakpoint.tableMax}) {
     font-size: 14px;
     margin: 0 6px;
   }
-  @media (max-width: ${({ theme }) => theme.breakpoint.tableMax}) {
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
     font-size: 12px;
-    margin: 0 4px;
-  }
-  @media (max-width: ${({ theme }) => theme.breakpoint.tableMin}) {
-    font-size: 10px;
     margin: 0 2px;
-    line-height: 24px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMin}) {
+    font-size: 10px;
+    margin: 0 auto;
     font-weight: 600;
   }
 `;
