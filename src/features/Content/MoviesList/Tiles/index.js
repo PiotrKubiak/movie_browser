ee1804ac21bbdcb711dbcf8827956c35a7b2be21
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-12-16 14:46:35 +0100

    added information react component

--- src/features/Content/MoviesList/Tiles/index.js
+++ src/features/Content/MoviesList/Tiles/index.js
@@ -1,32 +1,54 @@
-import { Container, Description, Image, Rate, Rating, Star, Tag, Tags, Title, Votes, Year } from "../../styledTile";
+import {
+  Container,
+  Description,
+  Image,
+  Information,
+  Rate,
+  Rating,
+  Star,
+  Tag,
+  Tags,
+  Title,
+  Votes,
+  Year,
+} from "../../styledTile";
 import { img_base_url } from "../../moviesBrowserApi";
 import { StyledTiles } from "../../styledMovies";
 
-
 const Tiles = ({ movieBrowser }) => {
-
-    return (
-        <StyledTiles>
-            {movieBrowser.map(({ id, poster_path, original_title, release_date, genre_ids, vote_average, vote_count }) => (
-                <Container key={id}>
-                    <Image src={img_base_url + poster_path} alt={original_title} />
-                    <Description>
-                        <Title> {original_title} </Title>
-                        {/* <Year>{release_date.substring(4,-1)}</Year> */}
-                        <Tags>
-                            <Tag>{genre_ids}</Tag>
-                        </Tags>
-
-                        <Rating>
-                            <Star />
-                            <Rate>{vote_average}</Rate>
-                            <Votes>{vote_count} votes</Votes>
-                        </Rating>
-                    </Description>
-                </Container>
-            ))}
-        </StyledTiles>
-    );
+  return (
+    <StyledTiles>
+      {movieBrowser.map(
+        ({
+          id,
+          poster_path,
+          original_title,
+          release_date,
+          genre_ids,
+          vote_average,
+          vote_count,
+        }) => (
+          <Container key={id}>
+            <Image src={img_base_url + poster_path} alt={original_title} />
+            <Information>
+              <Description>
+                <Title> {original_title} </Title>
+                {/* <Year>{release_date.substring(4, -1)}</Year> */}
+                <Tags>
+                  <Tag>{genre_ids}</Tag>
+                </Tags>
+              </Description>
+              <Rating>
+                <Star />
+                <Rate>{vote_average}</Rate>
+                <Votes>{vote_count} votes</Votes>
+              </Rating>
+            </Information>
+          </Container>
+        )
+      )}
+    </StyledTiles>
+  );
 };
 
 export default Tiles;
