4251d5e5db7f26baf5c35d39706ab813bf1606d0
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-12-19 11:57:46 +0100

    relocated file

--- src/features/Content/MoviesList/Tiles/MovieTile/Tile/index.js
+++ src/features/Content/MoviesList/Tiles/MovieTile/Tile/index.js
@@ -1,13 +1,13 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 const Tile = () => {
-    const { id } = useParams();
-
-
+    const {id} = useParams();
     return (
         <>
-            {id}
+<p>k</p>
+{id}
         </>
-    )
+    );
 };
+
 export default Tile;
