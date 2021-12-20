5db7c53d1c6a4637418a9cad448cd2f486656203
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-12-20 20:44:59 +0100

    getting data from API

--- src/features/Content/moviesBrowserApi.js
+++ src/features/Content/moviesBrowserApi.js
@@ -2,16 +2,17 @@ import axios from "axios";
 
 export const img_base_url = "https://image.tmdb.org/t/p/w500";
 const base_url = "https://api.themoviedb.org/3";
-const api_key = "?api_key=eb6efa05b2b8136a07d256a13fbb4f15&language=en-US&page=1";
+const api_key = "?api_key=eb6efa05b2b8136a07d256a13fbb4f15&language=en-US&page=5";
 const most_popular_url = "/movie/popular"
+// const api_key = "?api_key=eb6efa05b2b8136a07d256a13fbb4f15&language=en-US&page=";
 const popular_movies_api = base_url + most_popular_url + api_key;
 const popular_person_api = base_url + "/person/popular?api_key=eb6efa05b2b8136a07d256a13fbb4f15&language=en-US&page=1";
 const genres_url = "https://api.themoviedb.org/3/genre/movie/list?api_key=eb6efa05b2b8136a07d256a13fbb4f15&language=en-US";
 
 export const getMovies = () =>
     axios.get(popular_movies_api)
-        .then(response => response.data.results);
-
+        .then(response => response.data);
+// usuneliśmy po data .results
 export const getGenres = () =>
     axios.get(genres_url)
         .then(response => response.data.genres);
@@ -20,6 +21,13 @@ export const getPeople = () =>
     axios.get(popular_person_api)
         .then(response => response.data.results);
 
-export const getPage = () =>
-    axios.get(api_key)
+export const getPage = ({ page }) =>
+    axios.get(setPage(page))
         .then(response => response.data.page);
+
+export function setPage(page) {
+    if (page) {
+        const newUrl = { page };
+        return (newUrl)
+    }
+};
