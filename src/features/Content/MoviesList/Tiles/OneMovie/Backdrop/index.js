7f928136eda4fe916a894250baa02f649088ad7c
palel <32599663+palel@users.noreply.github.com>
2021-12-21 23:18:11 +0100

    Backdrop component

---
+++ src/features/Content/MoviesList/Tiles/OneMovie/Backdrop/index.js
@@ -0,0 +1,28 @@
+import { BackdropImage, Container, Gradient, Image, MainInfo, OtherInfo, Rate, RateScale, Rating, Star, Title } from "./styled";
+import posterBig2 from "./posterBig2.png";
+import reduxSaga from "redux-saga";
+
+const Backdrop = ({ backdropPath, title, vote_avg, num_vote }) => {
+
+    if (backdropPath) {
+        return (
+            <Container>
+                <BackdropImage bcg={backdropPath}>
+                    <Gradient>
+                        <MainInfo>
+                            <Title>{title}</Title>
+                            <Rating>
+                                <Star />
+                                <Rate>{vote_avg}</Rate>
+                                <OtherInfo>/ 10</OtherInfo>
+                                <OtherInfo>{num_vote} votes</OtherInfo>
+                            </Rating>
+                        </MainInfo>
+                    </Gradient>
+                </BackdropImage>
+            </Container>
+        );
+    } else return null;
+};
+
+export default Backdrop;
