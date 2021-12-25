be20f8de59b3b45c1f891c5e8804ea3be4d8ce56
palel <32599663+palel@users.noreply.github.com>
2021-12-26 00:25:56 +0100

    credits  added to content paramethers

--- src/features/Content/MoviePeople/content.js
+++ src/features/Content/MoviePeople/content.js
@@ -2,7 +2,7 @@ import Tile from "./Tile";
 import { Loader } from "../../../common/Loader";
 import { Error } from "../../../common/Error";
 
-export const Content = ({ movieBrowserStatus, movieBrowser }) => {
+export const Content = ({ movieBrowserStatus, movieBrowser, credits }) => {
   switch (movieBrowserStatus) {
     case "initial":
       return null;
@@ -11,7 +11,7 @@ export const Content = ({ movieBrowserStatus, movieBrowser }) => {
     case "error":
       return <Error />;
     case "success":
-      return <Tile movieBrowser={movieBrowser} />;
+      return <Tile movieBrowser={movieBrowser} credits={credits} />;
     default:
       throw new Error(`incorrect status: ${movieBrowserStatus}`);
   }
