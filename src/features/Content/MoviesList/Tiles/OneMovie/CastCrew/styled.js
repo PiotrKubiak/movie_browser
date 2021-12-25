46d694a98faa46ad9966e19928a725873c884cf8
palel <32599663+palel@users.noreply.github.com>
2021-12-26 00:26:46 +0100

    styles for CastCrew component

---
+++ src/features/Content/MoviesList/Tiles/OneMovie/CastCrew/styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { StyledTiles } from "../../../../styledTile";
+
+
+export const Section = styled.section`
+    max-width: 70%;
+    margin: 64px auto;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        max-width: 90%;      
+    }
+    
+`;
+
+export const Header = styled.header`
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 1.2;
+    margin-bottom: 32px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        font-size: 20px;
+    margin-bottom: 12px;
+
+    }
+`;
