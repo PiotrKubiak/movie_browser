95581198c4969e528aa52d1505e52390af88edd2
palel <32599663+palel@users.noreply.github.com>
2021-12-26 23:31:05 +0100

    Movie detail moved to separate component

--- src/features/Content/MoviesList/Tiles/OneMovie/index.js
+++ src/features/Content/MoviesList/Tiles/OneMovie/index.js
@@ -4,9 +4,9 @@ import { useParams } from "react-router-dom"
 import { Error } from "../../../../../common/Error";
 import { Loader } from "../../../../../common/Loader";
 import { img_base_url } from "../../../moviesBrowserApi";
-import { ImageMovie, Information, Rate, Rating, Star, Tag, Tags, Title, Votes, Year, Container, StyledTiles, OtherInfo, BasicInfo, Overview, SgColor, StyledInfo } from "./styled";
 import Backdrop from "./Backdrop";
 import CastCrew from "./CastCrew";
+import MovieDetails from "./MovieDetails";
 
 export function OneMovie() {
 
@@ -42,31 +42,7 @@ export function OneMovie() {
                 vote_avg={api.data.vote_average}
                 num_vote={api.data.vote_count}
               />
-
-              <StyledTiles>
-                <Container>
-                  <ImageMovie src={img_base_url + api.data.poster_path} alt='' />
-                  <Information>
-                    <Title> {api.data.original_title} </Title>
-                    <Year>{api.data.release_date.slice(0, 4)}</Year>
-                    <StyledInfo>
-                      {api.data.production_countries.map(({ name }) => { return <BasicInfo><SgColor>Production: </SgColor>{name}</BasicInfo> })}
-                      <BasicInfo><SgColor>Release date: </SgColor>{api.data.release_date}</BasicInfo>
-                    </StyledInfo>
-                    <Tags>
-                      {api.data.genres.map(({ name }) => { return <Tag>{name}</Tag> })}
-                    </Tags>
-                    <Rating>
-                      <Star />
-                      <Rate>{api.data.vote_average}</Rate>
-                      <OtherInfo> /10</OtherInfo>
-                      <Votes>{api.data.vote_count} votes</Votes>
-                    </Rating>
-                    <Overview>{api.data.overview}</Overview>
-                  </Information>
-                </Container>
-              </StyledTiles>
-
+              <MovieDetails img_base_url={img_base_url} api={api} />
               <CastCrew id={id} header="Cast" credits="cast" />
               <CastCrew id={id} header="Crew" credits="crew" />
             </>
