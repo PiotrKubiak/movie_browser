07d3ec363b59388bdb613a256f95d07b5d1d1e54
palel <32599663+palel@users.noreply.github.com>
2021-12-27 22:59:26 +0100

    shorten version without so many if statement

--- src/features/Content/MoviesList/Tiles/index.js
+++ src/features/Content/MoviesList/Tiles/index.js
@@ -1,101 +1,46 @@
 import { Description, Information, Rate, Rating, Star, Tag, Tags, Title, Votes, SubTitle, StyledLinkMovie, ImageMovie } from "../../styledTile";
 import { img_base_url } from "../../moviesBrowserApi";
 import { foundName } from "../getGenres";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { fetchGenres } from "../genresSlice";
 
 const Tile = ({ genres, movieBrowser, credits }) => {
-  let movieCastDisplay;
-  let movieCrewDisplay;
+  let movieDisplay;
+  let cast;
 
-  if (credits) {
-    movieCastDisplay = movieBrowser.data.cast.slice(0, 8);
-    movieCrewDisplay = movieBrowser.data.crew.slice(0, 8);
+  if (credits === "cast") {
+    movieDisplay = movieBrowser.data.cast.slice(0, 8);
+    cast = true;
+  } else if (credits === "crew") {
+    movieDisplay = movieBrowser.data.crew.slice(0, 8);
+    cast = false;
+  } else if (!credits) {
+    movieDisplay = movieBrowser;
   }
 
-  if (movieBrowser && !credits) {
-    return (
-      <>
-        {movieBrowser.map(({ id, poster_path, original_title, release_date, genre_ids = [], vote_average, vote_count }) => (
-          <StyledLinkMovie key={id} to={`/movies/${id}`}>
-            <ImageMovie src={img_base_url + poster_path} alt={original_title} />
-            <Information>
-              <Description>
-                <Title align="left"> {original_title} </Title>
-                <SubTitle>{release_date && release_date.slice(0, 4)}</SubTitle>
-                <Tags>
-                  {genre_ids.map((id, index) => {
-                    return <Tag key={index}>{foundName(id, genres)}</Tag>
-                  })}
-                </Tags>
-              </Description>
-              <Rating>
-                <Star />
-                <Rate>{vote_average}</Rate>
-                <Votes>{vote_count} votes</Votes>
-              </Rating>
-            </Information>
-          </StyledLinkMovie>
-        ))}
-      </>
-    )
-  };
-  if (movieBrowser && credits === "cast") {
-    return (
-      <>
-        {movieCastDisplay.map(({ id, poster_path, original_title, character, genre_ids = [], vote_average, vote_count }) => (
-          <StyledLinkMovie key={id} to={`/movies/${id}`}>
-            <ImageMovie src={img_base_url + poster_path} alt={original_title} />
-            <Information>
-              <Description>
-                <Title align="left"> {original_title} </Title>
-                <SubTitle>{character}</SubTitle>
-                <Tags>
-                  {genre_ids.map((id, index) => {
-                    return <Tag key={index}>{foundName(id, genres)}</Tag>
-                  })}
-                </Tags>
-              </Description>
-              <Rating>
-                <Star />
-                <Rate>{vote_average}</Rate>
-                <Votes>{vote_count} votes</Votes>
-              </Rating>
-            </Information>
-          </StyledLinkMovie>
-        ))}
-      </>
-    )
-  }
-
-  if (movieBrowser && credits === "crew") {
-    return (
-      <>
-        {movieCrewDisplay.map(({ id, poster_path, original_title, job, genre_ids = [], vote_average, vote_count }) => (
-          <StyledLinkMovie key={id} to={`/movies/${id}`}>
-            <ImageMovie src={img_base_url + poster_path} alt={original_title} />
-            <Information>
-              <Description>
-                <Title align="left"> {original_title} </Title>
-                <SubTitle>{job}</SubTitle>
-                <Tags>
-                  {genre_ids.map((id, index) => {
-                    return <Tag key={index}>{foundName(id, genres)}</Tag>
-                  })}
-                </Tags>
-              </Description>
-              <Rating>
-                <Star />
-                <Rate>{vote_average}</Rate>
-                <Votes>{vote_count} votes</Votes>
-              </Rating>
-            </Information>
-          </StyledLinkMovie>
-        ))}
-      </>
-    )
-  }
+  return (
+    <>
+      {movieDisplay.map(({ id, poster_path, original_title, release_date, character, job, genre_ids = [], vote_average, vote_count }) => (
+        <StyledLinkMovie key={id} to={`/movies/${id}`}>
+          <ImageMovie src={img_base_url + poster_path} alt={original_title} />
+          <Information>
+            <Description>
+              <Title align="left"> {original_title} </Title>
+              <SubTitle>{(character || job) ? (cast ? character : job) : release_date.slice(0, 4)}</SubTitle>
+              <Tags>
+                {genre_ids.map((id, index) => {
+                  return <Tag key={index}>{foundName(id, genres)}</Tag>
+                })}
+              </Tags>
+            </Description>
+            <Rating>
+              <Star />
+              <Rate>{vote_average}</Rate>
+              <Votes>{vote_count} votes</Votes>
+            </Rating>
+          </Information>
+        </StyledLinkMovie>
+      ))}
+    </>
+  )
 }
 
 export default Tile;
