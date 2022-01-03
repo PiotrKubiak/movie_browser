cf75a24267bb778af23836ecbd48253285c50fb1
Olaf Tarkowski <olaftarkowski97@gmail.com>
2022-01-03 13:27:50 +0100

    delete woodsmoke

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,6 @@ export const theme = {
     whisper: "#F5F5FA",
     mystic: "#E4E6F0",
     waterloo: "#7E839A",
-    woodsmoke: "#18181B",
     black: "#000000",
     blue: "#0044CC",
     blueHover: "#0255C5",
