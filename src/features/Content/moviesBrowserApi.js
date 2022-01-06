cfce80f439fa263d61b07501c40468d5200c7244
palel <32599663+palel@users.noreply.github.com>
2022-01-06 18:16:08 +0100

    details corrected

--- src/features/Content/moviesBrowserApi.js
+++ src/features/Content/moviesBrowserApi.js
@@ -4,29 +4,29 @@ import { pageNumber } from "../../common/Pagination/pageNumber";
 
 export const img_base_url = "https://image.tmdb.org/t/p/w1280";
 const base_url = "https://api.themoviedb.org/3";
-const api_key = "?api_key=eb6efa05b2b8136a07d256a13fbb4f15&page=";
+const api_key = "?api_key=eb6efa05b2b8136a07d256a13fbb4f15&language=en-US";
 const popular_movies_api = base_url + "/movie/popular" + api_key;
-const popular_person_api = base_url + "/person/popular" + api_key + pageNumber;
+const popular_person_api = base_url + "/person/popular" + api_key;
 const genres_url = base_url + "/genre/movie/list" + api_key;
-const moviesByQuery = base_url + "/search/movie" + api_key;
-const peopleByQuery = base_url + "/search/person" + api_key;
+const moviesByQuery = base_url + "search/movie" + api_key;
+const peopleByQuery = base_url + "search/person" + api_key;
 
 export const getMovies = () =>
-    axios.get(popular_movies_api + pageNumber)
-        .then(response => response.data.results);
-
-export const getPeople = () =>
-    axios.get(popular_person_api)
+    axios.get(popular_movies_api + "&page=" +pageNumber)
         .then(response => response.data.results);
 
 export const getGenres = () =>
     axios.get(genres_url)
         .then(response => response.data.genres);
 
+export const getPeople = () =>
+    axios.get(popular_person_api + "&page=" + pageNumber)
+        .then(response => response.data.results);
+
 export const getMoviesByQuery = (query) =>
-    axios.get( moviesByQuery + "&query=${query}" )
+    axios.get( moviesByQuery + `&query=${query}`)
         .then(response => response.data.results);
 
 export const getPeopleByQuery = (query) =>
-    axios.get(peopleByQuery + "&query=${query}")
+    axios.get(peopleByQuery + `&query=${query}`)
         .then(response => response.data.results);
