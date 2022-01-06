737b4a284bf6fb615ae979fd37ca1e1c3f970cd2
Piotr Kubiak <kuberxxx1986@gmail.com>
2022-01-06 21:32:53 +0100

    code formatted

--- src/features/Content/MoviesList/Tiles/OneMovie/Backdrop/index.js
+++ src/features/Content/MoviesList/Tiles/OneMovie/Backdrop/index.js
@@ -4,25 +4,25 @@ import { img_base_url } from "../../../../moviesBrowserApi";
 
 const Backdrop = ({ backdropPath, title, vote_avg, num_vote }) => {
 
-    if (backdropPath) {
-        return (
-            <Container>
-                <BackdropImage bcg={img_base_url + backdropPath}>
-                    <Gradient>
-                        <MainInfo>
-                            <Title>{title}</Title>
-                            <Rating>
-                                <Star />
-                                <Rate>{vote_avg}</Rate>
-                                <OtherInfo>/ 10</OtherInfo>
-                                <OtherInfo>{num_vote} votes</OtherInfo>
-                            </Rating>
-                        </MainInfo>
-                    </Gradient>
-                </BackdropImage>
-            </Container>
-        );
-    } else return null;
+  if (backdropPath) {
+    return (
+      <Container>
+        <BackdropImage bcg={img_base_url + backdropPath}>
+          <Gradient>
+            <MainInfo>
+              <Title>{title}</Title>
+              <Rating>
+                <Star />
+                <Rate>{vote_avg}</Rate>
+                <OtherInfo>/ 10</OtherInfo>
+                <OtherInfo>{num_vote} votes</OtherInfo>
+              </Rating>
+            </MainInfo>
+          </Gradient>
+        </BackdropImage>
+      </Container>
+    );
+  } else return null;
 };
 
 export default Backdrop;
