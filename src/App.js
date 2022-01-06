96a25eca12f77fd446ea5798bba421283792f924
palel <32599663+palel@users.noreply.github.com>
2022-01-06 18:02:07 +0100

    search engine

--- src/App.js
+++ src/App.js
@@ -3,18 +3,46 @@ import MoviePeople from "./features/Content/MoviePeople";
 import MoviesList from "./features/Content/MoviesList";
 import { Actress } from "./features/Content/MoviePeople/Tile/Actress";
 import { OneMovie } from "./features/Content/MoviesList/Tiles/OneMovie";
-import { ButtonsGroup, Camera, Container, Loupe, StyledBox, StyledInput, StyledItem, StyledList, StyledNav, StyledNavLink, StyledSearch, Title, } from "./styled";
+import {
+  ButtonsGroup,
+  Camera,
+  Container,
+  Loupe,
+  StyledBox,
+  StyledInput,
+  StyledItem,
+  StyledList,
+  StyledNav,
+  StyledNavLink,
+  StyledSearch,
+  Title
+} from "./styled";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { fetchMoviesByQuery } from "./features/Content/moviesBrowserSlice";
+import { fetchMoviesByQuery, fetchPeople, fetchPeopleByQuery, fetchPopularMovies } from "./features/Content/moviesBrowserSlice";
 
 function App() {
   const [query, setQuery] = useState("");
- 
+  console.log(window.location.hash.slice(2,));
+
   const dispatch = useDispatch();
-  
   useEffect(() => {
-    dispatch(fetchMoviesByQuery());
+    if (window.location.hash.slice(2,) === "movies") {
+      console.log(" jest w movies");
+      if (query.trim() === "") {
+        dispatch(fetchPopularMovies());
+      } else {
+        dispatch(fetchMoviesByQuery(query));
+      }
+    } else if (window.location.hash.slice(2,) === "people") {
+      console.log(" jest w people");
+      if (query.trim() === "") {
+        dispatch(fetchPeople());
+      } else {
+        dispatch(fetchPeopleByQuery(query));
+      }
+    }
+
   }, [dispatch, query]);
 
   return (
